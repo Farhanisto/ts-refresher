1137cecb37d3faab79415db49c65a02b66793011
Farhan <farhanisto@gmail.com>
2020-09-15 01:05:56 +0300

    code improvement

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,8 @@
 export class CustomMap {
   private googleMap: google.maps.Map
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  constructor(id: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(id), {
       center: {
         lat: 0,
         lng: 0
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import { CustomMap } from './CustomMap'
 console.log('hi farhan')
 console.log(new User())
 console.log(new Company())
-new CustomMap()
+new CustomMap('map')
